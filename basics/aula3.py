024a5c3987526888cef6c6db0397a70333abb601
keplinsousa <keplin.s.sousa@gmail.com>
2024-11-25 08:17:47 +0000

    inicio do curso, modulo basico

---
+++ basics/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('keplin')
+print(1, 'keplin "sousa"')
+
+# Aspas duplas
+print("keplin sousa")
+print(2, "keplin 'sousa'")
+
+# Escape
+print("keplin \"sousa\"")
+
+# r
+print(r"keplin \"sousa\"")
---
+++ basics/aula4.py
@@ -0,0 +1,21 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado
+# positivo.
+# print(11) # int
+# print(-11) # int
+# print(0)
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+# print(1.1, 10.11)
+# print(0.0, -1.5)
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor.
+print(type('keplin'))
+print(type(9))
+print(type(1.2), type(-2.8), type(0.0))
